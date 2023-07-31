a358f47bb7541286341d3128fbd05877a969d1be
Bogdddan <bogdankrajnik95@gmail.com>
2023-07-31 20:58:32 +0300

    step 3 completed

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 const contacts = require("./db/contacts.js");
 
-contacts.getContactById("AeHIrLTr6JkxGE6SN-0Rw").then(contacts => console.log(contacts)).catch(err => console.error(err));
-
-// contacts.read().then(console.log).catch(console.error);
+contacts.getContactById("AeHIrLTr6JkxGE6SN-0Rw").then(contacts => console.log(contacts)).catch(err => console.error(err));stwep 
